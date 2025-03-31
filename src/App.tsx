c254012d5cbd9bc0e68004c415d47bd78f30548c
valera <valeri.kuzmini123@gmail.com>
2025-03-31 20:27:04 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./scss/styles.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
