e7fc45d595aadb69e1b619bc43a513c815f16c01
valera <valeri.kuzmini123@gmail.com>
2025-04-05 17:48:04 +0400

    добавлен новый роут

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import "./scss/styles.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Products from "./components/Products";
+import { SelectedProduct } from "./components/SelectedProduct";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/products" element={<Products />}></Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SelectedProduct />} />
       </Routes>
     </BrowserRouter>
   );
