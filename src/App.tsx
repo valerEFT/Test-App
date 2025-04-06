d241947a5b5f4b55a6bbe3dde487043fb8ab4157
valera <valeri.kuzmini123@gmail.com>
2025-04-06 21:00:35 +0400

    заменил BrowserRouter на HashRouter для github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./scss/styles.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import Products from "./components/Products";
 import { SelectedProduct } from "./components/SelectedProduct";
 import CreateProduct from "./components/CreateProduct";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<SelectedProduct />} />
         <Route path="/create-product" element={<CreateProduct />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
